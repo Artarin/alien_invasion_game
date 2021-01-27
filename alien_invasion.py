2ac2a38f01bc8154570838f2e6c8e2ecb3c09084
Artyom Lipovoy <oskarualds@mail.ru>
2021-01-27 15:11:53 +0300

    add fps counter, fps control

--- alien_invasion.py
+++ alien_invasion.py
@@ -10,19 +10,21 @@ class AlienInvasion:
     def __init__(self):
         """game initialization and create game resources"""
         pygame.init()
-
-        self.settings = Settings()
+        self.settings = Settings(self)
         self.screen = pygame.display.set_mode(
             (self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Babilon-5. Alien invasion")
         self.ship = Ship(self)
-
+        self.old_time = pygame.time.get_ticks()
+        self.average_fps = []
+        self.max_fps = 60
     def run_game(self):
         """main game cycle"""
         while True:
             self._check_events()
             self.ship.update_position()
             self._update_screen()
+            self.fps_controller()
 
     def _check_events(self):
             # check keyboard and mouse
@@ -59,7 +61,22 @@ class AlienInvasion:
                     self.ship.moving_up = False
                 if event.key == pygame.K_DOWN:
                     self.ship.moving_down = False
-              
+    def fps_controller(self):
+        """ give a pause in game cycle, then it work to fast and print average fps"""
+        pygame.time.Clock().tick(self.max_fps)
+        self.new_time = pygame.time.get_ticks()
+        self.delta_time = self.new_time - self.old_time
+        
+        # if self.delta_time < 16:
+        #     pygame.time.wait(16 - self.delta_time)
+        self.summary_time = pygame.time.get_ticks() - self.old_time
+        self.old_time = self.new_time
+        self.average_fps.append(self.summary_time)
+        summ = 0
+        for average_fp in self.average_fps:
+            summ += average_fp
+        print(1000//(summ/len(self.average_fps)))
+        
 
     def _update_screen(self):
         # refresh display and drow objects
--- settings.py
+++ settings.py
@@ -1,11 +1,13 @@
 class Settings():
     """class for saving all serrings game alien invasion"""
 
-    def __init__(self):
+    def __init__(self, ai_game):
         """game settings initialization"""
         #screen settings
         self.screen_width = 800
         self.screen_height = 600
         self.bg_color = (14, 7, 57)
         # ship settings
-        self.ship_speed = 1.5
+        self.ship_speed = 2
+        # if self.delta_time > 16:
+            
